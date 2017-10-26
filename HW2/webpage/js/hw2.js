d53546b084c06ecdb216bd9aa45722d81fc69472
= <tdennis15@wou.edu>
2017-10-26 16:38:34 -0700

    HW2 commit 10/26 4:30

---
+++ HW2/webpage/js/hw2.js
@@ -0,0 +1,41 @@
+/*
+  A JavaScript file that will generate a table created from the input based in jQueryPage in HW2.
+  The inputs are a career choice, amount of exerience, and desired career length.
+*/
+
+const careers = ["Software Engineer", "Android Software Dev.","Cyber Security Eng","Information Security Eng."];
+var career;
+var exp;
+var tenure;
+
+function checkRadio(){
+  if($('#car1').prop('checked'))
+     career = careers[0];
+  if($('#car2').prop('checked'))
+     career = careers[1];
+  if($('#car3').prop('checked'))
+     career = careers[2];
+  if($('#car4').prop('checked'))
+     career = careers[3];
+  
+  exp = $('#exp').val();
+  tenure = $('#tenure').val();
+}
+  
+  
+/*
+  Build a table based upon the basic inputs provided, Defaulted to careers[1],
+*/
+
+$('#careerChoice').submit) {
+  checkRadio();
+  //$('careerTable').empty();
+  $('#careerTable').append(career);
+  //$('careerTable').apend(tenure);
+ // $('careerTable').apend(exp);
+})
+
+function careerTable(){
+  
+  
+}
